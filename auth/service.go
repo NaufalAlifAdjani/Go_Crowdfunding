2d46c768a855b125feb3a1c7a3ec34b638754009
NaufalAlifAdjani <naufalalifadjani12@gmail.com>
2024-12-18 20:05:01 +0700

    jwt auth, validate token

--- auth/service.go
+++ auth/service.go
@@ -1,15 +1,22 @@
 package auth
 
 import (
+	"errors"
 
 	"github.com/dgrijalva/jwt-go"
 )
 
 type Service interface {
 	GenerateToken(userID int) (string, error)
+	validateToken(token string) (*jwt.Token, error)
 }
 
 type jwtservice struct {
+}
+
+// validateToken implements Service.
+func (s *jwtservice) validateToken(token string) (*jwt.Token, error) {
+	panic("unimplemented")
 }
 
 var SECRET_KEY = []byte("BWASTARTUP_s3creT_k3Y")
@@ -32,3 +39,20 @@ func (s *jwtservice) GenerateToken(userID int) (string, error) {
 	return signedToken, nil
 }
 
+func (s *jwtservice) ValidateToken(encodedtoken string) (*jwt.Token, error) {
+	token, err := jwt.Parse(encodedtoken, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+
+		if !ok {
+			return nil, errors.New("Invalid Token")
+		}
+
+		return []byte(SECRET_KEY), nil
+	})
+
+	if err != nil {
+		return token, err
+	}
+
+	return token, nil
+}
